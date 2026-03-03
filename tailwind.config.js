8a141a6ee80be4df678b4f7ee54b79124a46f132
Tamam Krajem <tamamkarim71@gmail.com>
2026-03-03 17:21:53 +0200

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ['./index.ts', './src/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      borderWidth: {
+        hairline: '0.5px',
+      },
+    },
+  },
+  plugins: [],
+};
 module.exports = {
   content: [
     './App.{js,jsx,ts,tsx}',
